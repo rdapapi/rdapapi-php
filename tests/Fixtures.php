3abea13846d70eddacf10406aa68a116094432be
Nicolas Boutet <amd3002@gmail.com>
2026-05-12 09:11:27 +0200

    test: align phone fixtures and assertions with e.164 api response format

--- tests/Fixtures.php
+++ tests/Fixtures.php
@@ -20,7 +20,7 @@ final class Fixtures
                 'name' => 'MarkMonitor Inc.',
                 'iana_id' => '292',
                 'abuse_email' => 'abusecomplaints@markmonitor.com',
-                'abuse_phone' => '+1.2086851750',
+                'abuse_phone' => '+12086851750',
                 'url' => 'https://www.markmonitor.com',
             ],
             'dates' => [
@@ -162,7 +162,7 @@ final class Fixtures
             'name' => 'Google LLC',
             'organization' => 'Google LLC',
             'email' => 'arin-contact@google.com',
-            'phone' => '+1-650-253-0000',
+            'phone' => '+16502530000',
             'address' => '1600 Amphitheatre Parkway',
             'contact_url' => 'https://google.com',
             'country_code' => 'US',
